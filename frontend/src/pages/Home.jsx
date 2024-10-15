1e8d39b5460958984f4b091af69f73dc53889d93
ahmelevs <andrejs.hmelovs@gmail.com>
2024-10-15 17:05:09 +0400

    Notes on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,90 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+import Note from "../components/Note";
+import "../styles/Home.css";
+
 function Home() {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) {
+          // alert("Note deleted successfully");
+          setNotes((prev) => prev.filter((note) => note.id !== id));
+        } else {
+          alert("Failed to delete note");
+        }
+      })
+      .catch((err) => alert(err));
+  };
+
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { title, content })
+      .then((res) => {
+        if (res.status === 201) {
+          // alert("Note created successfully");
+        } else {
+          alert("Failed to create note");
+        }
+        getNotes();
+      })
+      .catch((err) => alert(err));
+  };
+
   return (
     <div>
-      <h1>Home</h1>
+      <div>
+        <h2>Notes</h2>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} onDelete={deleteNote} />
+        ))}
+      </div>
+      <h2>Create Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title:</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={title}
+          required
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <br />
+        <label htmlFor="content">Content:</label>
+        <br />
+        <textarea
+          id="content"
+          name="content"
+          value={content}
+          required
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
